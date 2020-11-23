fef5f64162ad9ecf119c05e0e58342a9e3b213a4
Diane Desira <diane.borg@mcast.edu.mt>
2020-11-23 16:31:50 +0100

    Created EnemyPathing and WaveConfig scripts
    EnemyPathing was set to make the enemy look as if it is following a path
    
    WaveConfig was created to avoid from having duplicate copies of data for each prefab. Instead the data will be shared and we will have only one copy.

---
+++ LaserDefender-42C/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints = new List<Transform>();// a collection to store different waypoints
+    //set in Unity. These waypoints will represent the path that the enemy needs to move along.
+
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0; // used to keep track of the current point we are working with. The index is the 
+    //position of the current waypoint.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* Setting the current enemy to be located on the first waypoint in the list.
+         * Different properties are fetched from their components. Our list is made up of just Transform components
+         * so each item is a trasform component, thus, waypoints[waypointIndex] will be translated to transform.
+         * That way, we just call the position property from this current transform. 
+         */
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMove();
+    }
+
+    void EnemyMove()
+    {
+        //checking if there are any more points in the list
+        if (waypointIndex < waypoints.Count) // if(waypointIndex <= waypoints.Count - 1)
+        {
+            float movementThisFrame = moveSpeed * Time.deltaTime; // making the enemy movement frame independent
+            Vector3 targetPosition = waypoints[waypointIndex].position;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++; // waypointIndex = waypointIndex + 1;
+            }
+        }
+        else // there are no more waypoints left in the list (all of them have been traversed)
+        {
+            /* The keyword gameObject refers to the object which has this current script attached to it. Therefore,
+             * we will be destroying the current enemy which has reached the end of the path.
+             */
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ LaserDefender-42C/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+    //the enemy
+    [SerializeField] GameObject enemyPrefab;
+
+    //the path on which to go
+    [SerializeField] GameObject pathPrefab;
+
+    //time between each enemy spawn
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+
+    //include this random time difference between spawns
+    [SerializeField] float spawnRandomFactor = 0.3f;
+
+    //number of enemies in the wave/group
+    [SerializeField] int numberOfEnemies = 5;
+
+    //enemy movement speed
+    [SerializeField] float enemyMoveSpeed = 2f;
+
+    /* By default all elements (variables, properties and methods) in a script have the 
+     * private access modifier. This is the most secure access modifier since the elements can
+     * only be accessed within the same script.
+     * The above variables, will be left private since once a script has access to them, 
+     * mistakes can be made and their values may be wrongly changed.
+     * Instead of giving full access to the variables, we will create getter methods (below)
+     * which will only return the value which is needed (without having access to the variable)
+     * Basically, we are setting these variables as READ ONLY.
+     */
+    
+    /*getter methods
+     * These methods need to be set as public so that they can be accessed by other scripts and
+     * thus, retrieve (read) the required value from the needed variable.
+     * Since they will be returning values, we need to indicate the return type in the method
+     * definition.
+    */
+    public GameObject GetEnemyPrefab()
+    {
+        return enemyPrefab;
+    }
+}
