cf6f43c1f1bc3d5fafb3715200d9c0353ac3d57d
Diane Desira <diane.borg@mcast.edu.mt>
2020-11-30 16:31:26 +0100

    Updated EnemySpawner and EnemyPathing, Created new enemy prefab, paths and waves
    EnemySpawner and EnemyPathing were updated so that the specific wave could be set for the current enemy clone.
    
    All the enemies in a wave were also generated.
    
    A new enemy prefab, 2 new paths and 2 new waves were created.

--- LaserDefender-42C/Assets/Scripts/EnemyPathing.cs
+++ LaserDefender-42C/Assets/Scripts/EnemyPathing.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] WaveConfig waveConfig;
+    WaveConfig waveConfig;
     
     List<Transform> waypoints = new List<Transform>();// a collection to store different waypoints
     //set in Unity. These waypoints will represent the path that the enemy needs to move along.
 
-    [SerializeField] float moveSpeed = 2f;
     int waypointIndex = 0; // used to keep track of the current point we are working with. The index is the 
     //position of the current waypoint.
 
@@ -36,7 +35,8 @@ public class EnemyPathing : MonoBehaviour
         //checking if there are any more points in the list
         if (waypointIndex < waypoints.Count) // if(waypointIndex <= waypoints.Count - 1)
         {
-            float movementThisFrame = moveSpeed * Time.deltaTime; // making the enemy movement frame independent
+            float movementThisFrame = waveConfig.GetEnemyMoveSpeed() * Time.deltaTime; 
+            // making the enemy movement frame independent
             Vector3 targetPosition = waypoints[waypointIndex].position;
 
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
@@ -53,5 +53,17 @@ public class EnemyPathing : MonoBehaviour
              */
             Destroy(gameObject);
         }
+    }
+    /* This method is required since different enemies are part of different waves so whenever a new enemy
+     * is cloned, this method will be called to set the wave for this enemy. It is vital that the Wave is
+     * set properly since the path on which this current enemy will move is found within the wave.
+     * This method has been declared as public since it needs to be called from OUTSIDE this script.
+     * A parameter is also required since the value of the wave will be different for different groups of 
+     * enemies AND the value of the correspoiding wave cannot be accessed from this script ( so we need it
+     * as input).
+     */
+    public void SetWaveConfig(WaveConfig waveConfigToSet)
+    {
+        waveConfig = waveConfigToSet;
     }
 }
--- LaserDefender-42C/Assets/Scripts/EnemySpawner.cs
+++ LaserDefender-42C/Assets/Scripts/EnemySpawner.cs
@@ -11,11 +11,31 @@ public class EnemySpawner : MonoBehaviour
     void Start()
     {
         WaveConfig currentWave = waveConfigs[startingWave];
+
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
+        {
+            GameObject enemyClone = Instantiate(waveConfig.GetEnemyPrefab(), 
+                                                waveConfig.GetWaypoints()[0].position, 
+                                                Quaternion.identity);
+
+            /* A reference to the current generated clone is fetched so that we can set the current wave
+             * inside its EnemyPathing component. Then, enemypathing can know from which wave the path
+             * should be fetched.
+             */
+            enemyClone.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
     }
 }
